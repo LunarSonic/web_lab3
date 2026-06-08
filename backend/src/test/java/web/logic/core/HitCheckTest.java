1670fbdc2e509f280f641c79a7712a44a9847444
Sonya Abdullaeva <sophiaabdullaeva2007@icloud.com>
2026-06-08 23:47:58 +0300

    some changes

--- backend/src/test/java/web/logic/core/HitCheckTest.java
+++ backend/src/test/java/web/logic/core/HitCheckTest.java
@@ -23,7 +23,8 @@ public class HitCheckTest {
             "-1.0, 1.1",
             "0.0, -2.0",
             "1.4, -1.4",
-            "1.1, -1.3"
+            "1.1, -1.3",
+            "0.0, 0.0"
     })
     void hits(float x, float y) {
         assertTrue(check(x, y));
